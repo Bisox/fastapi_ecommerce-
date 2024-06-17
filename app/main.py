91cdec3247851650aa6cbb6fffbcfac43b4d6fde
Evgen Bratenkov <giertrut@mail.ru>
2024-06-17 22:04:14 +0300

    added category, products, main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
+from itertools import product
+
 from fastapi import FastAPI
+
+from app.routers import category
+from app.routers import products
 
 app = FastAPI()
 
+app.include_router(category.router)
+app.include_router(products.router)
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def welcome() -> dict:
+    return {"message": "My e-commerce app"}
